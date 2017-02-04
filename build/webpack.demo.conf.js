5f879ac1238457fd054af7cc769bb81dde448364
njleonzhang <scream3616@sina.com>
2017-02-04 14:00:28 +0800

    bundle demo

---
+++ build/webpack.demo.conf.js
@@ -0,0 +1,40 @@
+var path = require('path')
+var config = require('../config')
+var utils = require('./utils')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var env = config.demo.env
+
+var webpackConfig = merge(baseWebpackConfig, {
+  entry: {
+    app: './example/main.js'
+  },
+  module: {
+    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  devtool: config.demo.productionSourceMap ? '#source-map' : false,
+  output: {
+    path: config.demo.assetsRoot,
+    filename: 'demo.js',
+  },
+  vue: {
+    loaders: utils.cssLoaders({
+      sourceMap: config.demo.productionSourceMap
+    })
+  },
+  plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'example/index.html',
+      inject: true
+    })
+  ]
+})
+
+module.exports = webpackConfig
--- config/index.js
+++ config/index.js
@@ -15,6 +15,19 @@ module.exports = {
     productionGzip: false,
     productionGzipExtensions: ['js', 'css']
   },
+  demo: {
+    env: require('./demo.env'),
+    assetsRoot: path.resolve(__dirname, '../demo-dist'),
+    assetsSubDirectory: '/',
+    assetsPublicPath: '/',
+    productionSourceMap: false,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css']
+  },
   dev: {
     env: require('./dev.env'),
     port: 8089,
