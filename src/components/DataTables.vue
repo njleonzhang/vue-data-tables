9703f1040ecdcea45a9fca58aa5ad52e940dbf89
罗尖 <luojian01@gridsum.com>
2017-07-26 15:44:40 +0800

    If the user uses @sort-change and :sortable="custom", he sorts himself

--- src/components/DataTables.vue
+++ src/components/DataTables.vue
@@ -273,6 +273,7 @@ export default {
       let sortedData = this.data.slice()
 
       if (this.sortData.order) {
+        if (this.sortData.column.sortable === 'custom' && this._events && this._events['sort-change']) return sortedData
         let order = this.sortData.order
         let prop = this.sortData.prop
         let isDescending = order === 'descending'
