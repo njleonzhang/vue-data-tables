0c77802e1f287f582e8f607c8d0b3ccf8ba30f4b
njleonzhang <scream3616@sina.com>
2017-09-15 09:58:50 +0800

    style adjustment

--- src/components/DataTables.vue
+++ src/components/DataTables.vue
@@ -8,167 +8,167 @@
 </template>
 
 <script>
-import ErrorTips from './ErrorTips.js'
-import ShareMixin from '../mixins/ShareMixin'
+  import ErrorTips from './ErrorTips.js'
+  import ShareMixin from '../mixins/ShareMixin'
 
-export default {
-  name: 'DataTables',
-  mixins: [ShareMixin],
-  data() {
-    return {
-      sortData: {}
-    }
-  },
-  computed: {
-    innerCheckboxFilterDef() {
-      let _allDataProps = this._allDataProps
-      return Object.assign({
-        props: undefined,
-        def: [],
-        colProps: {
-          span: 14
-        },
-        filterFunction: (el, filter) => {
-          let props = filter.props || _allDataProps
-          return props.some(prop => {
-            let elVal = el[prop]
-            /* istanbul ignore if */
-            if (elVal === undefined) {
-              console.error(ErrorTips.propError(prop))
-            } else if (elVal === null) {
-              return false
-            }
+  export default {
+    name: 'DataTables',
+    mixins: [ShareMixin],
+    data() {
+      return {
+        sortData: {}
+      }
+    },
+    computed: {
+      innerCheckboxFilterDef() {
+        let _allDataProps = this._allDataProps
+        return Object.assign({
+          props: undefined,
+          def: [],
+          colProps: {
+            span: 14
+          },
+          filterFunction: (el, filter) => {
+            let props = filter.props || _allDataProps
+            return props.some(prop => {
+              let elVal = el[prop]
+              /* istanbul ignore if */
+              if (elVal === undefined) {
+                console.error(ErrorTips.propError(prop))
+              } else if (elVal === null) {
+                return false
+              }
 
-            return filter.vals.some(val => {
-              return elVal.toString() === val
+              return filter.vals.some(val => {
+                return elVal.toString() === val
+              })
             })
-          })
-        }
-      }, this.checkboxFilterDef)
-    },
-    innerSearchDef() {
-      return Object.assign({
-        show: true,
-        props: undefined,
-        filterFunction: undefined,
-        debounceTime: 200
-      }, this.searchDef)
-    },
-    sortedData() {
-      let sortedData = this.data.slice()
-
-      if (this.sortData.order) {
-        let order = this.sortData.order
-        let prop = this.sortData.prop
-        let isDescending = order === 'descending'
-
-        // todo: customize sort function
-        sortedData.sort(function(a, b) {
-          if (a[prop] > b[prop]) {
-            return 1
-          } else if (a[prop] < b[prop]) {
-            return -1
-          } else {
-            return 0
           }
-        })
-        if (isDescending) {
-          sortedData.reverse()
-        }
-      }
+        }, this.checkboxFilterDef)
+      },
+      innerSearchDef() {
+        return Object.assign({
+          show: true,
+          props: undefined,
+          filterFunction: undefined,
+          debounceTime: 200
+        }, this.searchDef)
+      },
+      sortedData() {
+        let sortedData = this.data.slice()
 
-      return sortedData
-    },
-    tableData() {
-      let filteredData = this.sortedData.slice()
-      let _allDataProps = this._allDataProps
+        if (this.sortData.order) {
+          let order = this.sortData.order
+          let prop = this.sortData.prop
+          let isDescending = order === 'descending'
 
-      let doFilter = function(defaultFilterFunction, filter) {
-        let filterFunction = filter.filterFunction || defaultFilterFunction
-
-        filteredData = filteredData.filter(el => {
-          return filterFunction(el, filter)
-        })
-      }
-
-      this.filters.forEach(filter => {
-        let vals = filter.vals
-        if (!vals || vals.length === 0) {
-          return true
-        }
-
-        let defaultFilterFunction = function(el, filter) {
-          let props = filter.props || _allDataProps
-          return props.some(prop => {
-            let elVal = el[prop]
-            /* istanbul ignore if */
-            if (elVal === undefined) {
-              console.error(ErrorTips.propError(prop))
-            } else if (elVal === null) {
-              return false
+          // todo: customize sort function
+          sortedData.sort(function(a, b) {
+            if (a[prop] > b[prop]) {
+              return 1
+            } else if (a[prop] < b[prop]) {
+              return -1
+            } else {
+              return 0
             }
+          })
+          if (isDescending) {
+            sortedData.reverse()
+          }
+        }
 
-            return filter.vals.some(val => {
-              return elVal.toString().toLowerCase().indexOf(val.toLowerCase()) > -1
+        return sortedData
+      },
+      tableData() {
+        let filteredData = this.sortedData.slice()
+        let _allDataProps = this._allDataProps
+
+        let doFilter = function(defaultFilterFunction, filter) {
+          let filterFunction = filter.filterFunction || defaultFilterFunction
+
+          filteredData = filteredData.filter(el => {
+            return filterFunction(el, filter)
+          })
+        }
+
+        this.filters.forEach(filter => {
+          let vals = filter.vals
+          if (!vals || vals.length === 0) {
+            return true
+          }
+
+          let defaultFilterFunction = function(el, filter) {
+            let props = filter.props || _allDataProps
+            return props.some(prop => {
+              let elVal = el[prop]
+              /* istanbul ignore if */
+              if (elVal === undefined) {
+                console.error(ErrorTips.propError(prop))
+              } else if (elVal === null) {
+                return false
+              }
+
+              return filter.vals.some(val => {
+                return elVal.toString().toLowerCase().indexOf(val.toLowerCase()) > -1
+              })
             })
-          })
-        }
+          }
 
-        doFilter(defaultFilterFunction, filter)
-      })
+          doFilter(defaultFilterFunction, filter)
+        })
 
-      this.$emit('filtered-data', filteredData)
-      return filteredData
-    },
-    curTableData() {
-      let from = this.innerPageSize * (this.currentPage - 1)
-      let to = from + this.innerPageSize
-      return this.tableData.slice(from, to)
-    },
-    total() {
-      return this.tableData.length
-    },
-    filters() {
-      let filters = this.formatToArray(this.innerCustomFilters)
-      if (this.showActionBar) {
-        if (this.searchShow) {
-          filters.push({
-            props: this.formatProps(this.innerSearchDef.props),
-            vals: this.formatToArray(this.innerSearchKey),
-            filterFunction: this.innerSearchDef.filterFunction
-          })
+        this.$emit('filtered-data', filteredData)
+        return filteredData
+      },
+      curTableData() {
+        let from = this.innerPageSize * (this.currentPage - 1)
+        let to = from + this.innerPageSize
+        return this.tableData.slice(from, to)
+      },
+      total() {
+        return this.tableData.length
+      },
+      filters() {
+        let filters = this.formatToArray(this.innerCustomFilters)
+        if (this.showActionBar) {
+          if (this.searchShow) {
+            filters.push({
+              props: this.formatProps(this.innerSearchDef.props),
+              vals: this.formatToArray(this.innerSearchKey),
+              filterFunction: this.innerSearchDef.filterFunction
+            })
+          }
+          if (this.checkboxShow) {
+            filters.push({
+              props: this.formatProps(this.innerCheckboxFilterDef.props),
+              vals: this.checkBoxValues,
+              filterFunction: this.innerCheckboxFilterDef.filterFunction
+            })
+          }
         }
-        if (this.checkboxShow) {
-          filters.push({
-            props: this.formatProps(this.innerCheckboxFilterDef.props),
-            vals: this.checkBoxValues,
-            filterFunction: this.innerCheckboxFilterDef.filterFunction
-          })
-        }
+        return filters
       }
-      return filters
-    }
-  },
-  methods: {
-    handleSizeChange(size) {
-      this.innerPageSize = size
-      this.$emit('size-change', size)
     },
-    handleCurrentChange(currentPage) {
-      this.currentPage = currentPage
-      this.$emit('current-change', currentPage)
+    methods: {
+      handleSizeChange(size) {
+        this.innerPageSize = size
+        this.$emit('size-change', size)
+      },
+      handleCurrentChange(currentPage) {
+        this.currentPage = currentPage
+        this.$emit('current-change', currentPage)
+      },
+      handleFilterChange(checkBoxValues) {
+        this.checkBoxValues = checkBoxValues
+      }
     },
-    handleFilterChange(checkBoxValues) {
-      this.checkBoxValues = checkBoxValues
-    }
-  },
-  watch: {
-    data: {
-      immediate: true,
-      handler(val) {
-        this._allDataProps = Object.keys(val && val[0] || {})
+    watch: {
+      data: {
+        immediate: true,
+        handler(val) {
+          this._allDataProps = Object.keys(val && val[0] || {})
+        }
       }
     }
   }
-}
 </script>
--- src/components/DataTablesServer.vue
+++ src/components/DataTablesServer.vue
@@ -10,108 +10,108 @@
 </template>
 
 <script>
-import ShareMixin from '../mixins/ShareMixin'
+  import ShareMixin from '../mixins/ShareMixin'
 
-export default {
-  name: 'DataTablesServer',
-  mixins: [ShareMixin],
-  props: {
-    loadingStr: {
-      type: String,
-      default: ''
-    },
-    total: {
-      type: Number
-    },
-    loadData: {
-      type: Function
-    }
-  },
-  created() {
-    this.innerLoadData()
-  },
-  data() {
-    return {
-      lastData: {},
-      loading: false
-    }
-  },
-  computed: {
-    filters() {
-      let filters = this.formatToArray(this.innerCustomFilters)
-
-      if (this.showActionBar) {
-        if (this.searchShow) {
-          filters.push({
-            props: this.formatProps(this.innerSearchDef.props),
-            vals: this.formatToArray(this.innerSearchKey)
-          })
-        }
-        if (this.checkboxShow) {
-          filters.push({
-            props: this.formatProps(this.innerCheckboxFilterDef.props),
-            vals: this.checkBoxValues
-          })
-        }
+  export default {
+    name: 'DataTablesServer',
+    mixins: [ShareMixin],
+    props: {
+      loadingStr: {
+        type: String,
+        default: ''
+      },
+      total: {
+        type: Number
+      },
+      loadData: {
+        type: Function
       }
-
-      return filters
     },
-    curTableData() {
-      return this.data.length > this.innerPageSize
-        ? this.data.slice(0, this.innerPageSize)
-        : this.data
+    created() {
+      this.innerLoadData()
     },
-    queryInfo() {
+    data() {
       return {
-        currentPage: this.currentPage,
-        pageSize: this.innerPageSize,
-        sortData: this.sortData,
-        filters: this.filters
+        lastData: {},
+        loading: false
       }
-    }
-  },
-  methods: {
-    handleSizeChange(size) {
-      this.innerPageSize = size
-      this.$emit('query-change', {
-        type: 'sizeChange',
-        ...this.queryInfo
-      })
-
-      this.loadData && this.innerLoadData()
     },
-    handleCurrentChange(currentPage) {
-      this.currentPage = currentPage
-      this.$emit('query-change', {
-        type: 'pageChange',
-        ...this.queryInfo
-      })
+    computed: {
+      filters() {
+        let filters = this.formatToArray(this.innerCustomFilters)
 
-      this.loadData && this.innerLoadData()
-    },
-    handleFilterChange(checkBoxValues) {
-      this.checkBoxValues = checkBoxValues
-      this.$emit('query-change', {
-        type: 'checkBoxChange',
-        ...this.queryInfo
-      })
+        if (this.showActionBar) {
+          if (this.searchShow) {
+            filters.push({
+              props: this.formatProps(this.innerSearchDef.props),
+              vals: this.formatToArray(this.innerSearchKey)
+            })
+          }
+          if (this.checkboxShow) {
+            filters.push({
+              props: this.formatProps(this.innerCheckboxFilterDef.props),
+              vals: this.checkBoxValues
+            })
+          }
+        }
 
-      this.loadData && this.innerLoadData()
+        return filters
+      },
+      curTableData() {
+        return this.data.length > this.innerPageSize
+          ? this.data.slice(0, this.innerPageSize)
+          : this.data
+      },
+      queryInfo() {
+        return {
+          currentPage: this.currentPage,
+          pageSize: this.innerPageSize,
+          sortData: this.sortData,
+          filters: this.filters
+        }
+      }
     },
-    innerLoadData() {
-      this.loading = true
-      this.loadData(this.queryInfo, this.lastData)
-        .then(data => {
-          this.lastData = data
-          this.loading = false
-          this.$emit('load-data-success', data, this.queryInfo)
+    methods: {
+      handleSizeChange(size) {
+        this.innerPageSize = size
+        this.$emit('query-change', {
+          type: 'sizeChange',
+          ...this.queryInfo
         })
-        .catch(error => {
-          this.loading = false
-          this.$emit('load-data-fail', error, this.queryInfo)
+
+        this.loadData && this.innerLoadData()
+      },
+      handleCurrentChange(currentPage) {
+        this.currentPage = currentPage
+        this.$emit('query-change', {
+          type: 'pageChange',
+          ...this.queryInfo
         })
+
+        this.loadData && this.innerLoadData()
+      },
+      handleFilterChange(checkBoxValues) {
+        this.checkBoxValues = checkBoxValues
+        this.$emit('query-change', {
+          type: 'checkBoxChange',
+          ...this.queryInfo
+        })
+
+        this.loadData && this.innerLoadData()
+      },
+      innerLoadData() {
+        this.loading = true
+        this.loadData(this.queryInfo, this.lastData)
+          .then(data => {
+            this.lastData = data
+            this.loading = false
+            this.$emit('load-data-success', data, this.queryInfo)
+          })
+          .catch(error => {
+            this.loading = false
+            this.$emit('load-data-fail', error, this.queryInfo)
+          })
+      }
     }
   }
-}
 </script>
